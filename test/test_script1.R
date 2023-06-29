949ec9dfca3456b371a2b1eba5216fecc47d0e3d
Rob Lieberthal <50753146+rlieberthal@users.noreply.github.com>
2023-06-29 14:44:32 -0400

    Update test_script1.R

--- test/test_script1.R
+++ test/test_script1.R
@@ -1,12 +1,17 @@
-# Set the directory as the test directory
+# Set the working directory
 setwd('/Users/rlieberthal/Documents/PRIDIT-main/data')
+
 # Read in the test data
 data <- read.csv("test.csv")
+
 # Generate a vector of IDs
-ID.data <- data[,1]
+ID.data <- data[, 1]
+
 # Generate the ridit weights for the example data
 ridit.data <- ridit(data)
+
 # Generate the PRIDIT weights for the example data
 PRIDITweights.data <- PRIDITweight(ridit.data)
+
 # Generate the PRIDIT scores for the example data
-PRIDITscore.data <- PRIDITscore(ridit.data,ID.data,PRIDITweights.data)
+PRIDITscore.data <- PRIDITscore(ridit.data, ID.data, PRIDITweights.data)
