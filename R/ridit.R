1bd709c1e0236cdbd886bd21bb44e8be27abfec3
Rob Lieberthal <50753146+rlieberthal@users.noreply.github.com>
2023-06-29 14:26:17 -0400

    Update ridit.R

--- R/ridit.R
+++ R/ridit.R
@@ -11,30 +11,33 @@
 #'     \item{ID}{The unique identifier for each row.}
 #'     \item{DataMatrix}{A matrix containing additional data columns.}
 #'   }
-#' ridit(2)
 #' @export
-
-ridit <- function(allrawdata) { # allrawdata should have ID in the first column
-  IDvector <- allrawdata[,1]
-  rawdata <- data.matrix(allrawdata[,2:ncol(allrawdata)])
-  Fmat <- matrix(0,nrow(rawdata),ncol(rawdata))
-  Fmatmin <- matrix(0,nrow(rawdata),ncol(rawdata))
-  Fmatplu <- matrix(0,nrow(rawdata),ncol(rawdata))
-  bmat <- matrix(0,nrow(rawdata),ncol(rawdata))
-  for(i in 1:ncol(rawdata))
-  {Fn <- ecdf(rawdata[,i]) 
-  Fmatplu[,i] <- 1-Fn(rawdata[,i])
-  Fmatmin[,i] <- Fn(rawdata[,i]-0.001) # Make 0.001 the smallest possible incrememnt!
+ridit <- function(allrawdata) {
+  IDvector <- allrawdata[, 1]
+  rawdata <- data.matrix(allrawdata[, 2:ncol(allrawdata)])
+  Fmat <- matrix(0, nrow(rawdata), ncol(rawdata))
+  Fmatmin <- matrix(0, nrow(rawdata), ncol(rawdata))
+  Fmatplu <- matrix(0, nrow(rawdata), ncol(rawdata))
+  bmat <- matrix(0, nrow(rawdata), ncol(rawdata))
+  
+  for (i in 1:ncol(rawdata)) {
+    Fn <- ecdf(rawdata[, i]) 
+    Fmatplu[, i] <- 1 - Fn(rawdata[, i])
+    Fmatmin[, i] <- Fn(rawdata[, i] - 0.001)  # Make 0.001 the smallest possible increment!
   }
-  Bij <- Fmatmin[,1:ncol(rawdata)]-Fmatplu[,1:ncol(rawdata)]
-  for(j in 1:ncol(Bij))
-  {Bij.df <- data.frame(Bij[,j])
-  Bij.vec <- data.matrix(Bij.df)
-  Bij.vec[is.na(Bij.vec)] <- 0
-  Bij[,j] <- Bij.vec
+  
+  Bij <- Fmatmin[, 1:ncol(rawdata)] - Fmatplu[, 1:ncol(rawdata)]
+  
+  for (j in 1:ncol(Bij)) {
+    Bij.df <- data.frame(Bij[, j])
+    Bij.vec <- data.matrix(Bij.df)
+    Bij.vec[is.na(Bij.vec)] <- 0
+    Bij[, j] <- Bij.vec
   }
-  # write.table(Bij, file = "Bij.csv", sep = ",", col.names = NA)
+  
   Bij.data.frame <- data.frame(Bij)
-  colnames(Bij.data.frame) <- colnames(allrawdata[,2:ncol(allrawdata)])
-  Bij.data.frame <- data.frame(Claim.ID=IDvector,Bij.data.frame)
+  colnames(Bij.data.frame) <- colnames(allrawdata[, 2:ncol(allrawdata)])
+  Bij.data.frame <- data.frame(Claim.ID = IDvector, Bij.data.frame)
+  
+  return(Bij.data.frame)
 }
