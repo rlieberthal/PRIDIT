1adaff7c8228bb197d780b830cb77d00fb122765
Rob Lieberthal, PhD <50753146+rlieberthal@users.noreply.github.com>
2025-06-25 20:44:48 -0400

    Create PRIDITPackage.R

---
+++ R/PRIDITPackage.R
@@ -0,0 +1,8 @@
+#' pridit: Principal Component Analysis Applied to Ridit Scoring
+#'
+#' The pridit package provides functions for implementing the PRIDIT
+#' (Principal Component Analysis applied to RIDITs) scoring system.
+#'
+#' @docType package
+#' @name pridit
+NULL
